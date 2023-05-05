3d68027c82c2009b01b0ccfb855d5b169b0af9b1
Allan Conti Junior <58399481+allancontij@users.noreply.github.com>
2023-05-05 18:23:26 -0300

    Criação do Layout e inicialização dos componentes.

--- app/src/main/java/devandroid/allan/appqrcodekotlin/MainActivity.kt
+++ app/src/main/java/devandroid/allan/appqrcodekotlin/MainActivity.kt
@@ -2,10 +2,29 @@ package devandroid.allan.appqrcodekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    var editQRCode: EditText? = null
+    var btnGerar: Button? = null
+    var imgQRCode: ImageView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initComponentes()
+
+    }
+
+    fun initComponentes() {
+
+        editQRCode = findViewById(R.id.editQRCode)
+        btnGerar = findViewById(R.id.btnGerar)
+        imgQRCode = findViewById(R.id.imgQRCode)
+
     }
 }
