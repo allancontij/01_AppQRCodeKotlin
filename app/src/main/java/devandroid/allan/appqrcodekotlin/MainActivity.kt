2cc276db7c28f4800247572c681c7ddb8414cb7e
Allan Conti Junior <58399481+allancontij@users.noreply.github.com>
2023-05-08 09:31:47 -0300

    Implementação do metodo gerarQRCode e finalização do projeto.

--- app/src/main/java/devandroid/allan/appqrcodekotlin/MainActivity.kt
+++ app/src/main/java/devandroid/allan/appqrcodekotlin/MainActivity.kt
@@ -1,10 +1,16 @@
 package devandroid.allan.appqrcodekotlin
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,6 +23,50 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initComponentes()
+
+        btnGerar!!.setOnClickListener {
+            if (TextUtils.isEmpty(editQRCode!!.text.toString())){
+
+                editQRCode!!.error = "*"
+                editQRCode!!.requestFocus()
+
+            } else {
+
+                gerarQRCode(editQRCode!!.text.toString())
+
+            }
+        }
+
+    }
+
+    fun gerarQRCode(conteudoQRCode: String) {
+
+        val qrCode = QRCodeWriter()
+
+        try {
+
+            val bitMatrix = qrCode.encode(conteudoQRCode, BarcodeFormat.QR_CODE, 196, 196)
+            val tamanho = bitMatrix.width
+            val altura = bitMatrix.height
+
+            val bitmap = Bitmap.createBitmap(tamanho, altura, Bitmap.Config.RGB_565)
+
+            for (x in 0 until tamanho){
+
+                for (y in 0 until altura){
+
+                    bitmap.setPixel(x, y, if(bitMatrix[x,y]) Color.BLACK else Color.WHITE)
+
+                }
+            }
+
+            imgQRCode!!.setImageBitmap(bitmap)
+
+        }catch (e: WriterException){
+
+
+
+        }
 
     }
 
